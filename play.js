6c2c1e4998e5702a32c174d02822581b9c60315d
Brian LaFond <brian.d.lafond@gmail.com>
2021-02-10 18:13:41 +0000

    refactor to add client file

--- play.js
+++ play.js
@@ -1,24 +1,4 @@
-const net = require('net');
-const { stdin } = require('process');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-    const conn = net.createConnection({
-        host: '135.23.222.131',
-        port: 50542
-    });
-
-    conn.setEncoding('utf8'); // interpret incoming data as text
-
-    conn.on('data', function(data) { // sends data to the server
-        console.log(data);
-    })
-
-    return conn;
-
-}
+const connect = require('./client'); //importing connect function
 
 console.log('Connecting ...');
 connect();
